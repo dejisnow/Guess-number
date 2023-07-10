48c4e159a8d257162fb90e85f3160c2de1b9f8b0
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2023-07-10 22:28:19 +0100

    Update guessNum.js
    fixed a bug in a function

--- guessNum.js
+++ guessNum.js
@@ -1,21 +1,21 @@
 window.onload=()=>{
-let display= document.querySelector(".display")
+let display= document.querySelector(".display");
     let input =
-    document.getElementById("input")
-    let btn = document.querySelector(".btn")
-    let box = document.querySelector(".box")
-    let notification= document.querySelector(".notification")
-    let trails = document.querySelector(".trials")
+    document.getElementById("input");
+    let btn = document.querySelector(".btn");
+    let box = document.querySelector(".box");
+    let notification= document.querySelector(".notification");
+    let trails = document.querySelector(".trials");
     
-    let score = document.querySelector(".score")
+    let score = document.querySelector(".score");
    
     
     
 let guessCount = 3;
 let scores = 0;
 
- var num = Math.floor(Math.random() * 9)+ 1
- var randomNum = Math.floor(Math.random() * 8)+ 1
+ var num = Math.floor(Math.random() * 9)+ 1;
+ var randomNum = Math.floor(Math.random() * 8)+ 1;
 
 trails.innerHTML = `${guessCount}`
   display.innerHTML =`<h2> Guess a number <= ${num + randomNum } </h2>`
@@ -27,37 +27,35 @@ trails.innerHTML = `${guessCount}`
         console.log(num)
         if(value === num){
         scores+=100
-        score.innerHTML = `${scores}`
-        notification.style.display = "block"
-        notification.style.color = "green"
+        score.innerHTML = `${scores}`;
+        notification.style.display = "block";
+        notification.style.color = "green";
        
-        notification.innerHTML = "You guessed it 👏🎉🎉!!"
+        notification.innerHTML = "You guessed it 👏🎉🎉!!";
        input.disabled = true;
-            trails.innerHTML = "0"
-            btn.removeEventListener("click",btnEvent)
+            trails.innerHTML = "0";
+            btn.removeEventListener("click",btnEvent);
      
         }else{
-         notification.style.display = "block"
-        notification.style.color = "red"
+         notification.style.display = "block";
+        notification.style.color = "red";
         
         guessCount--
     if(guessCount > 0 ){
         notification.innerHTML = `<samp>Failed guess🥲 <br> you have ${guessCount} guesses left!! </samp>` }
         else{
             notification.style.display = "none"
-            box.style.color = "red"
-            box.innerHTML = "GAME OVER !!"
-            input.value = ""
+            box.style.color = "red";
+            box.innerHTML = "GAME OVER !!";
+            input.value = "";
             input.disabled = true;
-            trails.innerHTML = "0"
-            btn.removeEventListener("click",btnEvent)
+            trails.innerHTML = "0";
+            btn.removeEventListener("click",btnEvent);
         }
         
-            
         }
         
-        input.value = ""
+        input.value = "";
         
     })
-    
 }             
